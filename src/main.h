af5c513ef0b3fb53b719b1d18fbbd1b2826fee0b
awais3344 <awais3344@gmail.com>
2014-05-02 13:11:39 +0500

    ver update, final release

--- src/main.h
+++ src/main.h
@@ -25,7 +25,6 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 52;
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
